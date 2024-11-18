1cf7290ea1e48a2ef2d9310dddfa55cb8f424e85
Simone <simone.giacomelli@gmail.com>
2024-11-18 14:56:35 +0000

    remove deprecated

--- src/wwwpy/remote/designer/rpc.py
+++ src/wwwpy/remote/designer/rpc.py
@@ -1,26 +1,16 @@
 from pathlib import Path
 from typing import List, Any
 
-from wwwpy.common import modlib, files
+from wwwpy.common import files
 
 
 class DesignerRpc:
-    # todo deprecated
-    def package_file_changed_sync(self, package_name: str, events: List[Any]):
-        from wwwpy.common.filesystem.sync import sync_delta2
-        from wwwpy.common.filesystem.sync import Sync
-        sync_impl: Sync = sync_delta2
-        directory = modlib._find_package_directory(package_name)
-        sync_impl.sync_target(directory, events)
-
-        from wwwpy.remote.browser_main import _reload
-        _reload()
 
     def hotreload_notify_changes(self, events: List[Any]):
-        from wwwpy.common.filesystem.sync import sync_delta2
-        from wwwpy.common.filesystem.sync import Sync
-        sync_impl: Sync = sync_delta2
         directory = Path(files._bundle_path)
+
+        from wwwpy.common.filesystem.sync import Sync, sync_delta2
+        sync_impl: Sync = sync_delta2
         sync_impl.sync_target(directory, events)
 
         from wwwpy.remote.browser_main import _reload
