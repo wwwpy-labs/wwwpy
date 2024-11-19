a246474b26c244a8a486c14c777425f51349992f
Simone <simone.giacomelli@gmail.com>
2024-11-19 10:32:04 +0000

    add reloader_test.py

--- tests/common/reloader_test.py
+++ tests/common/reloader_test.py
@@ -52,3 +52,67 @@ def test_simple_reload__of_exec(dyn_sys_path: DynSysPath):
     exec('import p1', gl, lo)
     assert lo['p1'].b == 123
     assert not hasattr(lo['p1'], 'a')
+
+def test_simple_import_of_package_module(dyn_sys_path: DynSysPath):
+    # GIVEN
+    dyn_sys_path.write_module2('server/__init__.py', '')
+    dyn_sys_path.write_module2('server/rpc.py', 'b = 2')
+    # import server.rpc as rpc
+    # assert rpc.b == 2
+    gl = {}
+    lo = {}
+    exec('import server.rpc', gl, lo)
+    assert lo['server'].rpc.b == 2
+
+    # WHEN
+    unload_path(str(dyn_sys_path.path))
+
+    # THEN
+    dyn_sys_path.write_module2('server/rpc.py', 'b = 42')
+    gl = {}
+    lo = {}
+    exec('import server.rpc', gl, lo)
+    assert lo['server'].rpc.b == 42
+    assert not hasattr(lo['server'].rpc, 'a')
+
+
+def test_import_x_as_y(dyn_sys_path: DynSysPath):
+    # GIVEN
+    dyn_sys_path.write_module2('server/__init__.py', '')
+    dyn_sys_path.write_module2('server/rpc.py', 'b = 2')
+    # import server.rpc as rpc
+    # assert rpc.b == 2
+    gl = {}
+    lo = {}
+    exec('import server.rpc as rpc', gl, lo)
+    assert lo['rpc'].b == 2
+
+    # WHEN
+    unload_path(str(dyn_sys_path.path))
+
+    # THEN
+    dyn_sys_path.write_module2('server/rpc.py', 'b = 42')
+    # import server.rpc as rpc
+    # assert rpc.b == 3
+    # assert not hasattr(rpc, 'a')
+    gl = {}
+    lo = {}
+    exec('import server.rpc as rpc', gl, lo)
+    assert lo['rpc'].b == 42
+    assert not hasattr(lo['rpc'], 'a')
+
+def test_from_x_import_y(dyn_sys_path: DynSysPath):
+    # GIVEN
+    dyn_sys_path.write_module2('server/__init__.py', '')
+    dyn_sys_path.write_module2('server/rpc.py', 'b = 2')
+    from server import rpc
+    assert rpc.b == 2
+
+    # WHEN
+    unload_path(str(dyn_sys_path.path))
+
+    # THEN
+    dyn_sys_path.write_module2('server/rpc.py', 'b = 42')
+    from server import rpc
+    assert rpc.b == 42
+    assert not hasattr(rpc, 'a')
