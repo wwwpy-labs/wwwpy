af653e068669cc4074483548cb0cbfeedd5739a6
Simone Giacomelli <simone.giacomelli@gmail.com>
2024-11-19 21:04:16 +0000

    Updated version to 0.1.48 build meta

--- src/wwwpy/_build_meta.py
+++ src/wwwpy/_build_meta.py
@@ -1,3 +1,3 @@
-__version__ = "0.1.47"
-git_hash_short = "7bd991c"
-git_hash = "7bd991cae185ee2f41d7472ed9f4e366cb076d93"
+__version__ = "0.1.48"
+git_hash_short = "efe1a80"
+git_hash = "efe1a80d0afd8a9e98862ff5f4289dd8e7aa5cb0"
