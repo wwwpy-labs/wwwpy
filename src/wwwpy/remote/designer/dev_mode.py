fb061e5c156d883291aa5238f5263eda53269c84
Simone <simone.giacomelli@gmail.com>
2024-11-24 20:09:05 +0000

    add async loop global exception handler

--- src/wwwpy/remote/designer/dev_mode.py
+++ src/wwwpy/remote/designer/dev_mode.py
@@ -1,6 +1,11 @@
 from __future__ import annotations
 
+import asyncio
+import logging
+
 import js
+
+logger = logging.getLogger(__name__)
 
 _wwwpy_dev_mode = 'wwwpy_dev_mode'
 
@@ -19,5 +24,18 @@ async def activate():
     from wwwpy.remote.designer import log_redirect
     log_redirect.redirect_logging()
 
+    loop = asyncio.get_event_loop()
+    loop.set_exception_handler(_global_exception_handler)
+
+
 def is_active() -> bool:
     return hasattr(js.window, _wwwpy_dev_mode)
+
+
+def _global_exception_handler(loop, context):
+    # The context parameter contains details about the exception
+    logger.info(f"Global handler caught: {context['message']}")
+    exception = context.get('exception')
+    if exception:
+        logger.info(f"Exception type: {type(exception)}, Args: {exception.args}")
+        logger.exception(exception)
