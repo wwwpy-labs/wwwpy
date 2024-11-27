3dc7f182cee2bbc6943df98a968bcb067a36c6f7
simone <simone.giacomelli@gmail.com>
2024-11-27 21:10:50 +0000

    Updated version to 0.1.59

--- src/wwwpy/_build_meta.py
+++ src/wwwpy/_build_meta.py
@@ -1,3 +1,3 @@
-__version__ = "0.1.58"
-git_hash_short = "ababb52"
-git_hash = "ababb52be780ed96151b60552d84d4415cbc2d39"
+__version__ = "0.1.59"
+git_hash_short = "04c172e"
+git_hash = "04c172e12375db1ce9175f8f6c03815b84cdffaa"
