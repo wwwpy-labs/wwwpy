11598cddfcbb368c578b212c761ec8ba22550423
simone <simone.giacomelli@gmail.com>
2024-11-30 19:32:00 +0000

    asgi - tornado ok with websocket

--- src/wwwpy/asgi/tornado_asgi_handler.py
+++ src/wwwpy/asgi/tornado_asgi_handler.py
@@ -1,8 +1,6 @@
 from typing import Any
-
-from tornado.web import RequestHandler
+import asyncio
 from tornado.websocket import WebSocketHandler
-
 import logging
 
 logger = logging.getLogger(__name__)
@@ -24,8 +22,69 @@ class AsgiHandler(WebSocketHandler):
         await super().get(*args, **kwargs)
 
     def open(self):
-        logger.debug("open")
+        logger.debug("WebSocket connection opened")
+        self.receive_queue = asyncio.Queue()
+        asyncio.create_task(self._run_asgi_app())
 
+    async def _run_asgi_app(self):
+        scope = {
+            "type": "websocket",
+            "asgi": {"version": "3.0"},
+            "http_version": self.request.version,
+            "scheme": self.request.protocol,
+            "path": self.request.path,
+            "raw_path": self.request.path.encode(UTF8),
+            "query_string": self.request.query.encode(UTF8),
+            "headers": self._collect_headers(),
+            "client": (self.request.remote_ip, 0),
+            "server": self.request.host,
+            "subprotocols": self.request.headers.get_list("Sec-WebSocket-Protocol"),
+        }
+
+        async def receive():
+            return await self.receive_queue.get()
+
+        async def send(message):
+            message_type = message.get('type')
+            if message_type == 'websocket.send':
+                if 'text' in message:
+                    await self.write_message(message['text'])
+                elif 'bytes' in message:
+                    await self.write_message(message['bytes'], binary=True)
+            elif message_type == 'websocket.close':
+                code = message.get('code', 1000)
+                reason = message.get('reason', '')
+                await self.close(code=code, reason=reason)
+            else:
+                logger.error(f"Unsupported ASGI message type: {message_type}")
+
+        try:
+            await self._asgi_app(scope, receive, send)
+        except Exception as e:
+            logger.error(f"Error in ASGI application: {e}")
+            await self.close()
+
+    def on_message(self, message):
+        asyncio.create_task(self._queue_message(message))
+
+    async def _queue_message(self, message):
+        if isinstance(message, bytes):
+            await self.receive_queue.put({
+                'type': 'websocket.receive',
+                'bytes': message
+            })
+        else:
+            await self.receive_queue.put({
+                'type': 'websocket.receive',
+                'text': message
+            })
+
+    def on_close(self):
+        logger.debug("WebSocket connection closed")
+        asyncio.create_task(self._queue_disconnect())
+
+    async def _queue_disconnect(self):
+        await self.receive_queue.put({'type': 'websocket.disconnect'})
 
     async def post(self, *args: Any, **kwargs: Any):
         await self._handle_http_request(args, kwargs)
@@ -38,7 +97,8 @@ class AsgiHandler(WebSocketHandler):
             "method": self.request.method,
             "query_string": self.request.query.encode(UTF8),
             "headers": self._collect_headers(),
-            "client": (self.request.remote_ip, 0)
+            "client": (self.request.remote_ip, 0),
+            "server": self.request.host,
         }
 
         async def receive():
@@ -57,7 +117,7 @@ class AsgiHandler(WebSocketHandler):
                 self.write(data['body'])
             else:
                 raise RuntimeError(
-                    f"Unsupported response type \"{data['type']}\" for asgi app")
+                    f"Unsupported response type \"{data['type']}\" for ASGI app")
 
         await self._asgi_app(scope, receive, send)
 
