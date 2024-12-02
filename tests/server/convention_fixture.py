363e2d84d6739b3bec7b5fb7fddf43ce8944d175
simone <simone.giacomelli@gmail.com>
2024-12-02 16:43:12 +0000

    cleanup convention_fixture.py

--- tests/server/convention_fixture.py
+++ tests/server/convention_fixture.py
@@ -2,16 +2,16 @@ from __future__ import annotations
 
 from pathlib import Path
 
+from wwwpy.common.settingslib import Settings
 from wwwpy.server.configure import Project, setup
 from wwwpy.server.convention import default_config, add_project
-from wwwpy.server.settingslib import user_settings
 from wwwpy.webserver import Webserver
 
 
 def start_test_convention(directory: Path, webserver: Webserver = None, dev_mode=False) -> Project:
     config = default_config(directory, dev_mode)
-    project = setup(config, user_settings())
-    add_project(project)
+    project = setup(config, Settings())
+
     if webserver is not None:
         webserver.set_http_route(*project.routes)
 
