8a3f302afb3ac8bf80a52546c30dfe29849608af
simone <simone.giacomelli@gmail.com>
2025-01-21 15:22:09 +0000

    databind prototyping - refactoring

--- tests/remote/databind/test_databind.py
+++ tests/remote/databind/test_databind.py
@@ -3,7 +3,6 @@ from dataclasses import dataclass
 
 import js
 from js import document
-from pyodide.ffi import create_proxy
 
 from tests.server.rpc4tests import rpctst_exec
 from wwwpy.common.databind.databind import Binding
@@ -25,12 +24,10 @@ class Car:
 async def test_databind_input_string1():
     # GIVEN
     tag1 = _new_docu_input_tag1()
-
     user = User('foo1')
 
     # WHEN
-    target = Binding(user, 'name', InputTargetAdapter(tag1))
-    target.apply_binding()
+    _bind(user, 'name', tag1)
 
     # THEN
     assert tag1.value == 'foo1'
@@ -39,12 +36,10 @@ async def test_databind_input_string1():
 async def test_databind_input_string2():
     # GIVEN
     tag1 = _new_docu_input_tag1()
-
     car1 = Car('yellow')
 
     # WHEN
-    target = Binding(car1, 'color', InputTargetAdapter(tag1))
-    target.apply_binding()
+    _bind(car1, 'color', tag1)
 
     # THEN
     assert tag1.value == 'yellow'
@@ -53,12 +48,10 @@ async def test_databind_input_string2():
 async def test_databind_input_string__target_to_source():
     # GIVEN
     tag1 = _new_docu_input_tag1()
-
     car1 = Car('')
 
     # WHEN
-    target = Binding(car1, 'color', InputTargetAdapter(tag1))
-    target.apply_binding()
+    _bind(car1, 'color', tag1)
 
     await rpctst_exec("page.locator('#tag1').press_sequentially('yellow1')")
 
@@ -70,3 +63,8 @@ def _new_docu_input_tag1():
     document.body.innerHTML = '<input id="tag1">'
     tag1: js.HTMLInputElement = document.getElementById('tag1')  # noqa
     return tag1
+
+
+def _bind(instance, attr_name, tag1):
+    target = Binding(instance, attr_name, InputTargetAdapter(tag1))
+    target.apply_binding()
