d65b89137d12e8c655f8733e5f4c7023ca2fd3d0
simone <simone.giacomelli@gmail.com>
2025-02-02 11:43:34 +0000

    changed technique to send logs to server - fix wrong order

--- src/wwwpy/remote/designer/log_redirect.py
+++ src/wwwpy/remote/designer/log_redirect.py
@@ -4,23 +4,24 @@ from js import console
 from wwwpy.common.designer import log_emit
 from wwwpy.server.designer import rpc
 
-_log_buffer = []
+_log_buffer = asyncio.Queue()
 
 
 async def _process_buffer():
-    """This techique avoids the issue of wrong log order"""
-    loc = _log_buffer.copy()
-    _log_buffer.clear()
-
-    for msg in loc:
+    """This technique avoids the issue of wrong log order"""
+    while True:
+        msg = await _log_buffer.get()
         await rpc.server_console_log(msg)
 
 
 def redirect_logging():
+    asyncio.create_task(_process_buffer())
+
     def emit(msg: str):
         console.log(msg)
-        _log_buffer.append(msg)
-        asyncio.create_task(_process_buffer())
+        # this is not correct because we are not in a coroutine
+        # but it probably works because we are in the browser with just one event loop
+        _log_buffer.put_nowait(msg)
 
     log_emit.add_once(emit)
     log_emit.warning_to_log()
