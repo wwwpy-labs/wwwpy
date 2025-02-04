9b0d7b1ca78bf85eb3400a34f5f61bc8081ff8f0
simone <simone.giacomelli@gmail.com>
2025-02-04 13:06:14 +0000

    add setup_shoelace_async

--- src/wwwpy/remote/shoelace.py
+++ src/wwwpy/remote/shoelace.py
@@ -1,23 +1,43 @@
 from js import document
 import js
 
+from wwwpy.common.asynclib import create_task_safe
+from wwwpy.remote.jslib import script_load_once
+
+_version = '2.20.0'
+_css_url = f'https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@{_version}/cdn/themes/dark.css'
+_js_url = f'https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@{_version}/cdn/shoelace.js'
+
+_task = []
+
+
 def setup_shoelace():
+    if not _task:
+        task = create_task_safe(setup_shoelace_async())
+        _task.append(task)
+    return _task[0]
+
+
+
+async def setup_shoelace_async():
     document.documentElement.className = 'sl-theme-dark'
     document.head.append(document.createRange().createContextualFragment(_head_style))
+    await script_load_once(_js_url, type='module')
 
 
+# <script type="module" src="https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.20.0/cdn/shoelace.js" ></script>
 
 # language=html
-_head_style = """
+_head_style = f"""
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
-<style>@import 'https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.15.1/cdn/themes/dark.css';</style>
+<style>@import '{_css_url}';</style>
 <style>
-    body {
+    body {{
         font: 16px sans-serif;
         background-color: var(--sl-color-neutral-0);
         color: var(--sl-color-neutral-900);
         padding: 1rem;
-    }
+    }}
 </style>
-<script type="module" src="https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.15.1/cdn/shoelace.js"></script>
 """
+_head_script = f"""<script type="module" src="{_js_url}"></script>"""
