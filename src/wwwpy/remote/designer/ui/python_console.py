1e5a09950e6df12854dcf621c767108395d0c472
simone <simone.giacomelli@gmail.com>
2025-02-19 11:36:28 +0000

    python console - small improvements

--- src/wwwpy/remote/designer/ui/python_console.py
+++ src/wwwpy/remote/designer/ui/python_console.py
@@ -12,19 +12,20 @@ from wwwpy.common import state
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
+
 @dataclass
 class State:
-    python_code: str = ''
+    python_code: str = 'logger.info("Hello")'
 
 
-class PythonConsoleComponent(wpc.Component, tag_name='component-1'):
+class PythonConsoleComponent(wpc.Component, tag_name='wwwpy-python-console'):
     _ta_python_code: js.HTMLTextAreaElement = wpc.element()
 
     def init_component(self):
         # language=html
         self.element.innerHTML = """
-<div>Run Python code:</div>
-<textarea data-name="_ta_python_code" placeholder="Press CTRL-Enter to run code" rows="10" wrap="off" 
+<div>Press CTRL-Enter to run Python code</div>
+<textarea data-name="_ta_python_code" rows="12" wrap="off" 
 style="width: 95%;"></textarea>
 """
         self._state = state._restore(State)
@@ -32,10 +33,7 @@ style="width: 95%;"></textarea>
 
     async def _ta_python_code__keydown(self, event):
         if event.ctrlKey and event.key == 'Enter':
-            await js.pyodide.runPythonAsync(
-                self._ta_python_code.value,
-                globals=globals()
-            )
+            await js.pyodide.runPythonAsync(self._ta_python_code.value, globals=globals())
 
     async def _ta_python_code__input(self, event):
         self._state.python_code = self._ta_python_code.value
