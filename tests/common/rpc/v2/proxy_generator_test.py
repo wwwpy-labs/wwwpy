75673538d8b9cca19911cb557802b8e4f4901280
simone <simone.giacomelli@gmail.com>
2025-02-25 10:06:48 +0000

    enhance proxy_generator_test: refactor tests to use db_fake for setup and improve readability

--- tests/common/rpc/v2/proxy_generator_test.py
+++ tests/common/rpc/v2/proxy_generator_test.py
@@ -37,34 +37,34 @@ source_async = source.replace('def ', 'async def ')
 
 def test_instantiation(db_fake):
     # WHEN
-    gen = proxy_generator.generate(source, DispatcherFake)
+    gen = db_fake.generate(source)
     exec(gen)
 
     # THEN
     assert len(DispatcherFake.instances) == 1
 
 
-def test_function_definitions():
+def test_function_definitions(db_fake):
     # WHEN
-    gen = proxy_generator.generate(source, DispatcherFake)
+    gen = db_fake.generate(source)
 
     # THEN
     assert 'def add(a: int, b: int) -> int:' in gen
     assert 'def sub(a: int, b: int) -> int:' in gen
 
 
-def todo_test_async_function_definitions():
+def todo_test_async_function_definitions(db_fake):
     # WHEN
-    gen = proxy_generator.generate(source_async, DispatcherFake)
+    gen = db_fake.generate(source_async)
 
     # THEN
     assert 'async def add(a: int, b: int) -> int:' in gen
     assert 'async def sub(a: int, b: int) -> int:' in gen
 
 
-def test_function_type_hints():
+def test_function_type_hints(db_fake):
     # WHEN
-    gen = proxy_generator.generate('def add(a: int, b: int = 123) -> int: pass', DispatcherFake)
+    gen = db_fake.generate('def add(a: int, b: int = 123) -> int: pass')
 
     # THEN
     assert 'def add(a: int, b: int=123) -> int:' in gen
@@ -72,40 +72,36 @@ def test_function_type_hints():
 
 def test_definition_complete_called(db_fake):
     # WHEN
-    gen = proxy_generator.generate(source, DispatcherFake)
+    gen = db_fake.generate(source)
     exec(gen)
 
     # THEN
-    builder = DispatcherFake.instances[0]
-    assert len(builder.definition_complete_invokes) == 1
-    invoke = builder.definition_complete_invokes[0]
+    assert len(db_fake.builder.definition_complete_invokes) == 1
+    invoke = db_fake.builder.definition_complete_invokes[0]
     assert invoke.target == TargetType.module
 
 
 def test_module_function_generation(db_fake):
     # WHEN
-    gen = proxy_generator.generate(source, DispatcherFake)
+    gen = db_fake.generate(source)
     exec(gen)
 
     # THEN
-    builder = DispatcherFake.instances[0]
-    locals_ = builder.definition_complete_invokes[0].locals_
+    locals_ = db_fake.builder.definition_complete_invokes[0].locals_
     assert 'add' in locals_
     assert 'sub' in locals_
 
 
-def test_function_return_value(db_fake, dyn_sys_path: DynSysPath):
+def test_function_return_value(db_fake):
     # GIVEN
-    gen = proxy_generator.generate(source, DispatcherFake)
-    dyn_sys_path.write_module2('module1.py', gen)
+    db_fake.generate(source, module='module1')
 
     import module1  # fires the instantiation of the builder
 
     def dispatch_module_function(*args):
         return 42
 
-    builder = DispatcherFake.instances[0]
-    builder.dispatch_module_function = dispatch_module_function
+    db_fake.builder.dispatch_module_function = dispatch_module_function
 
     # WHEN invoke add
     res = module1.add(1, 2)
@@ -114,10 +110,9 @@ def test_function_return_value(db_fake, dyn_sys_path: DynSysPath):
     assert res == 42
 
 
-def test_function_args_values_and_type_hint(db_fake, dyn_sys_path: DynSysPath):
+def test_function_args_values_and_type_hint(db_fake):
     # GIVEN
-    gen = proxy_generator.generate(source, DispatcherFake)
-    dyn_sys_path.write_module2('module1.py', gen)
+    db_fake.generate(source, module='module1')
 
     import module1  # fires the instantiation of the builder
 
@@ -126,8 +121,7 @@ def test_function_args_values_and_type_hint(db_fake, dyn_sys_path: DynSysPath):
         assert args == [(1, int), (2, int)]
         return 'ignored'
 
-    builder = DispatcherFake.instances[0]
-    builder.dispatch_module_function = dispatch_module_function
+    db_fake.builder.dispatch_module_function = dispatch_module_function
 
     # WHEN invoke add
     module1.add(1, 2)
@@ -159,8 +153,7 @@ def test_function_args_values_and_type_hint__complex_type_fromimport_clause(db_f
         assert args == [(person, Person)]
         return 'ignored'
 
-    builder = DispatcherFake.instances[0]
-    builder.dispatch_module_function = dispatch_module_function
+    db_fake.builder.dispatch_module_function = dispatch_module_function
 
     # WHEN invoke add
     module1.fun1(person)
@@ -183,8 +176,7 @@ def test_function_args_values_and_type_hint__complex_type_simple_import(db_fake)
         assert args == [(person, Person)]
         return 'ignored'
 
-    builder = DispatcherFake.instances[0]
-    builder.dispatch_module_function = dispatch_module_function
+    db_fake.builder.dispatch_module_function = dispatch_module_function
 
     # WHEN invoke add
     module1.fun1(person)
@@ -197,10 +189,16 @@ class DbFake:
     def __init__(self, dyn_sys_path: DynSysPath):
         self.dyn_sys_path = dyn_sys_path
 
-    def generate(self, source: str, module=None):
+    def generate(self, source: str, module=None) -> str:
         gen = proxy_generator.generate(source, DispatcherFake)
         if module is not None:
             self.dyn_sys_path.write_module2(f'{module}.py', gen)
+        return gen
+
+    @property
+    def builder(self):
+        assert len(DispatcherFake.instances) == 1
+        return DispatcherFake.instances[0]
 
 
 @pytest.fixture
