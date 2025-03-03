6c8cf64f5480cb58cbdf14ef02acafb334baf5e3
simone <simone.giacomelli@gmail.com>
2025-03-03 13:31:23 +0000

    refactor generate_stub function and clean up unused code

--- src/wwwpy/common/rpc2/stub_generator.py
+++ src/wwwpy/common/rpc2/stub_generator.py
@@ -1,9 +1,10 @@
 from __future__ import annotations
 
+import ast
 import types
 from types import SimpleNamespace
+from typing import Optional
 
-from wwwpy.common.designer import code_info
 from wwwpy.common.rpc2.encoder_decoder import EncoderDecoder
 from wwwpy.common.rpc2.transport import Transport
 
@@ -37,39 +38,23 @@ class Stub:
         """This must setup the namespace for the classes"""
 
 
-class StubGenerator:
-    """Generates a stub source code from the given source code.
-The goals are:
-- forward the function/method calls to the stub
-- generate the module_init call at the end of the generated source code
-- preserve the type hints and default parameters of the functions/methods
-- preserve the import and importFrom statements so the type hints are available
-    """
-
-    def parse(self, source: str) -> code_info.Info:
-        return code_info.info(source)
-
-    def generate(self, source: str, stub_type: type[Stub], stub_args: str = '') -> str:
-        """Parses the given source code and generates a new source code that:
-        - calls module_init at the end the generated source code
-        - calls dispatch_sync/dispatch_async for each function/method
-        - removes the implementation of the functions and replaces it with a forwarding call to the stub
-
-        Inclusion/Exclusion
-        - MUST generate also a class __init__ method
-        - MUST NOT generate entities (function/method/class) that starts with '_'
-        """
-
-
-import ast
-from typing import Optional
-
 _annotations_type = set[ast.Name]
 
 
 # def caller_proxy_generate(source: str, dispatcher_callable: Type[Dispatcher], dispatcher_args: str = '') -> str:
 # def generate_stub(source: str, stub_type: type[Stub], stub_args: str = '') -> str:
 def generate_stub(source: str, stub_type: type[Stub], stub_args: str = '') -> str:
+    """Generates a stub source code from the given source code.
+The goals are:
+- forward the function/method calls to the stub
+- preserve the type hints and default parameters of the functions/methods
+- preserve the import and importFrom statements so the type hints are available
+- removes the implementation of the functions and replaces it with a forwarding call to the stub
+
+Inclusion/Exclusion
+- MUST NOT generate entities (function/method/class) that starts with '_'
+    """
+
     tree: ast.Module = ast.parse(source)
     module = stub_type.__module__
     qualified_name = stub_type.__qualname__
@@ -109,7 +94,6 @@ def generate_stub(source: str, stub_type: type[Stub], stub_args: str = '') -> st
             lines[idx] = ast.unparse(line) if _is_import_used(line, used_annotations) else ''
         elif isinstance(line, ast.ImportFrom):
             lines[idx] = ast.unparse(line) if _is_import_from_used(line, used_annotations) else ''
-
 
     # setup_functions call
     lines.append(f'dispatcher.setup_functions({", ".join(functions_names)})')
