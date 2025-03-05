60526f67209812b8751172c87a5d6bed24ad4de9
simone <simone.giacomelli@gmail.com>
2025-03-05 11:43:30 +0000

    refactor Transport class to raise NotImplementedError for unimplemented methods

--- src/wwwpy/common/rpc2/transport.py
+++ src/wwwpy/common/rpc2/transport.py
@@ -3,10 +3,10 @@ from __future__ import annotations
 
 class Transport:
 
-    def send_sync(self, payload: str | bytes): ...
+    def send_sync(self, payload: str | bytes): raise NotImplementedError
 
-    async def send_async(self, payload: str | bytes): ...
+    async def send_async(self, payload: str | bytes): raise NotImplementedError
 
-    def recv_sync(self) -> str | bytes: ...
+    def recv_sync(self) -> str | bytes: raise NotImplementedError
 
-    async def recv_async(self) -> str | bytes: ...
+    async def recv_async(self) -> str | bytes: raise NotImplementedError
