0d58e6b39e8633351f1d3ae4255f6075065f9945
simone <simone.giacomelli@gmail.com>
2025-03-13 16:59:44 +0000

    Updated version to 0.1.73

--- src/wwwpy/_build_meta.py
+++ src/wwwpy/_build_meta.py
@@ -1,3 +1,3 @@
-__version__ = "0.1.72"
-git_hash_short = "4b3e83a"
-git_hash = "4b3e83a44457e8e1f7b041f9e9ad01882dc8d0a9"
+__version__ = "0.1.73"
+git_hash_short = "61a35c3"
+git_hash = "61a35c3a2e44ca1f90c7267e0b551f8c8c34dc99"
