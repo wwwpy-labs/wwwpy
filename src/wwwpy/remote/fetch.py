bd11406c35df9b47a93dac06f19a84de4424e85d
simone <simone.giacomelli@gmail.com>
2025-03-21 18:59:15 +0000

    fix: improve logging for async_fetch_str by showing data length and truncating output

--- src/wwwpy/remote/fetch.py
+++ src/wwwpy/remote/fetch.py
@@ -1,6 +1,6 @@
-from js import fetch
-
 import logging
+
+from js import fetch
 
 logger = logging.getLogger(__name__)
 
@@ -8,7 +8,8 @@ logger = logging.getLogger(__name__)
 async def async_fetch_str(url: str, method: str = 'GET', data: str = '') -> str:
     logger.debug(f'url={url}')
     logger.debug(f'method={method}')
-    logger.debug(f'data=`{data}`')
+    logger.debug(f'len(data)={len(data)}')
+    logger.debug(f'data=`{data[:100]}`')
     response = await fetch(url, method=method, body=data)
     text = await response.text()
     return text
