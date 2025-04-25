a0dc5cab67248ecc85f648f2a7e8822daf6747a9
simone <simone.giacomelli@gmail.com>
2025-04-25 13:46:52 +0100

    refactor(element_path): enhance element_path function to improve shadow DOM handling and path construction

--- src/wwwpy/remote/designer/element_path.py
+++ src/wwwpy/remote/designer/element_path.py
@@ -14,23 +14,50 @@ logger = logging.getLogger(__name__)
 
 
 def element_path(element: Element) -> ElementPath | None:
-    """Returns an instance of  """
+    """Returns an instance of ElementPath that describes the path to the element"""
 
+    # Start building the path
     path = []
-    while element:
-        if is_instance_of(element, js.ShadowRoot):
-            element = element.host
-        component = get_component(element)
+    current = element
+
+    while current:
+        # Check if we've reached a component
+        component = get_component(current)
         if component:
             clazz = component.__class__
+            # We found the component, return the path
             return ElementPath(clazz.__module__, clazz.__name__, path, Origin.live)
-        if element == document.body:
+
+        # Reached document body without finding a component
+        if current == document.body:
             return None
 
-        parent = element.parentNode
-        child_index = Array.prototype.indexOf.call(parent.children, element) if parent else -1
-        attributes = {attr.name: attr.value for attr in element.attributes}
-        path.insert(0, Node(element.tagName.lower(), child_index, attributes))
-        element = parent
+        # Handle shadow root - move to host element
+        if is_instance_of(current, js.ShadowRoot):
+            current = current.host
+            continue
+
+        # Get the parent to determine next step
+        parent = current.parentNode
+        if not parent:
+            return None
+
+        # Add the current node to the path
+        add_node_to_path(current, parent, path)
+
+        if hasattr(current, "assignedSlot") and current.assignedSlot:
+            add_node_to_path(parent, parent.parentNode, path)
+            current = parent.parentNode
+            continue
+
+        # Move up to parent for next iteration
+        current = parent
 
     return None
+
+
+def add_node_to_path(node, parent, path):
+    """Helper to add a node to the path with its attributes and position"""
+    child_index = Array.prototype.indexOf.call(parent.children, node) if parent else -1
+    attributes = {attr.name: attr.value for attr in node.attributes}
+    path.insert(0, Node(node.tagName.lower(), child_index, attributes))
--- tests/remote/designer/element_path_test.py
+++ tests/remote/designer/element_path_test.py
@@ -1,11 +1,15 @@
+import js
 from js import document
 
+import wwwpy.remote.component as wpc
 from tests.common import dyn_sys_path
+from tests.remote.remote_fixtures import clean_document
 from wwwpy.common.designer.html_locator import Node
+from wwwpy.remote import dict_to_js
 from wwwpy.remote.designer.element_path import element_path
 
 
-def test_target_path_to_component(tmp_path, dyn_sys_path):
+def test_target_path_to_component(tmp_path, dyn_sys_path, clean_document):
     # GIVEN
     dyn_sys_path.write_module('', 'component1.py', '''import js
 
@@ -24,7 +28,7 @@ class Component1(wpc.Component):
     """
     ''')
 
-    from component1 import Component1
+    from component1 import Component1  # noqa
     component1 = Component1()
 
     document.body.innerHTML = ''
@@ -45,7 +49,7 @@ class Component1(wpc.Component):
     assert actual.path == path
 
 
-def test_target_path__without_component():
+def test_target_path__without_component(clean_document):
     # GIVEN
 
     document.body.innerHTML = """
@@ -85,3 +89,79 @@ def test_target_path__unattached_piece_of_dom():
 
     # THEN
     assert actual is None
+
+
+async def test_component_with_shadow_root(clean_document):
+    # GIVEN (one component with shadow root)
+    # we don't need two
+    class ShadowWpc(wpc.Component):
+        div1: js.HTMLElement = wpc.element()
+
+        def init_component(self):
+            self.element.attachShadow(dict_to_js({'mode': 'open'}))
+            self.element.shadowRoot.innerHTML = """<br><div data-name="div1">shadow</div>"""
+
+    shadow_wpc = ShadowWpc()
+    document.body.append(shadow_wpc.element)
+
+    # WHEN
+    actual = element_path(shadow_wpc.div1)
+
+    # THEN
+    expected_path = [Node("div", 1, {'data-name': 'div1'})]
+    assert actual is not None
+    # assert actual.class_module == 'component1'  # this file itself
+    assert actual.class_name == 'ShadowWpc'
+    assert actual.path == expected_path, f'actual.path=```\n{actual.path}\n``` != ```\n{expected_path}\n```'
+
+
+class TestSlottedComponent:
+    class SlottedWpc(wpc.Component, tag_name='slotted-wpc'):
+        def init_component(self):
+            self.element.attachShadow(dict_to_js({'mode': 'open'}))
+            self.element.shadowRoot.innerHTML = '<slot></slot>'
+
+    class RootWpc(wpc.Component, tag_name='root-wpc'):
+        inner: js.HTMLElement = wpc.element()
+
+        def init_component(self):
+            self.element.attachShadow(dict_to_js({'mode': 'open'}))
+            # we avoid naming the component so we don't clash with other tests
+            # snippet = (SlottedWpc.component_metadata
+            #            .build_snippet({'data-name': 'slotted1'}, '<span><div data-name="inner">inner-div</div></span>'))
+            html = """<br><slotted-wpc data-name="slotted1"><div data-name="inner">inner-div</div></slotted-wpc>"""
+            self.element.shadowRoot.innerHTML = html
+
+    async def test_class_name(self, clean_document):
+        # GIVEN
+
+        root_wpc = self.RootWpc()
+        document.body.append(root_wpc.element)
+
+        # WHEN
+        actual = element_path(root_wpc.inner)
+
+        # THEN
+        assert actual is not None
+
+        assert actual.class_module == __name__  # it is the current module itself
+        assert actual.class_name != 'SlottedWpc'
+        assert actual.class_name == 'RootWpc'
+
+    async def test_path(self, clean_document):
+        # GIVEN
+
+        root_wpc = self.RootWpc()
+        document.body.append(root_wpc.element)
+
+        # WHEN
+        actual = element_path(root_wpc.inner)
+
+        # THEN
+        expected_path = [
+            Node("slotted-wpc", 1, {'data-name': 'slotted1'}),
+            Node("div", 0, {'data-name': 'inner'})]
+
+        # await asyncio.sleep(1000000)
+        assert actual is not None
+        assert actual.path == expected_path, f'actual.path=```\n{actual.path}\n``` != ```\n{expected_path}\n```'
