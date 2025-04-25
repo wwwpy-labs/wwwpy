03ea6fb4d5eeffc06d3cb14d5a666552ddd7392d
simone <simone.giacomelli@gmail.com>
2025-04-25 09:38:17 +0100

    feat(rpc4tests): prototyping

--- tests/remote/rpc4tests_test.py
+++ tests/remote/rpc4tests_test.py
@@ -1,4 +1,3 @@
-import asyncio
 import logging
 from dataclasses import dataclass
 
@@ -8,7 +7,6 @@ from pyodide.ffi import create_proxy, create_once_callable
 
 from tests.remote.remote_fixtures import clean_document
 from tests.remote.rpc4tests_helper import rpctst_exec_touch_event
-from wwwpy.remote.jslib import waitAnimationFrame
 
 logger = logging.getLogger(__name__)
 
@@ -45,7 +43,7 @@ async def test_touch_apis(div1):
     _verify_xy(events[1], x + 20, y + 20)
 
 
-async def test_pointer_apis__cancel(div1):
+async def test_pointer_apis__pointercancel(div1):
     # GIVEN
     rect = div1.getBoundingClientRect()
     x = rect.x + rect.width / 2
@@ -74,8 +72,7 @@ async def test_pointer_apis__cancel(div1):
     assert targets == ['div1', 'div1', 'div1']
 
 
-
-async def test_touch_on_pointer_api(div1):
+async def test_pointer_apis__pointerup(div1):
     # GIVEN
     rect = div1.getBoundingClientRect()
     x = rect.x + rect.width / 2
@@ -84,183 +81,30 @@ async def test_touch_on_pointer_api(div1):
 
     events = []
     div1.addEventListener('pointerdown', create_proxy(events.append))
-    # div1.addEventListener('pointermove', create_proxy(events.append))
-    div1.addEventListener('pointermove',
-                          create_proxy(lambda e: [events.append(e), e.target.setPointerCapture(e.pointerId), ]))
-    div1.addEventListener('pointerup', create_proxy(events.append))
-
-    # WHEN
-    await rpctst_exec_touch_event([
-        {
-            'type': 'touchStart',
-            'touchPoints': [{
-                'x': x, 'y': y,
-                'radiusX': 2.5, 'radiusY': 2.5,
-                'rotationAngle': 0,
-                'force': 1,
-                'id': 1
-            }],
-            'modifiers': 0
-        },
-        {
-            'type': 'touchMove',
-            'touchPoints': [{
-                'x': x + 20, 'y': y + 20,
-                'radiusX': 2.5, 'radiusY': 2.5,
-                'rotationAngle': 0,
-                'force': 1,
-                'id': 1
-            }],
-            'modifiers': 0
-        },
-        {
-            'type': 'touchEnd',
-            'touchPoints': [],
-            'modifiers': 0
-        }
-    ])
-    # THEN
-    types = list(map(lambda e: e.type, events))
-    assert types == ['pointerdown', 'pointermove', 'pointerup']
-    targets = list(map(lambda e: e.target.id, events))
-    assert targets == ['div1', 'div1', 'div1']
-    _verify_xy(events[0], x, y)
-    _verify_xy(events[1], x + 20, y + 20)
-
-
-async def test_touch_on_pointer_api_chatgpt(div1):
-    rect = div1.getBoundingClientRect()
-    x = rect.x + rect.width / 2
-    y = rect.y + rect.height / 2
-
-    # div1.style.touchAction = 'none'
-    events = []
-
-    def on_down(e):
-        e.target.setPointerCapture(e.pointerId)
-        events.append(e)
-
-    def on_up(e):
-        e.target.releasePointerCapture(e.pointerId)
-        events.append(e)
-    div1.addEventListener('pointerdown', create_proxy(on_down))
     div1.addEventListener('pointermove', create_proxy(events.append))
-    div1.addEventListener('pointerup', create_proxy(on_up))
+    div1.addEventListener('pointerup', create_proxy(events.append))
     div1.addEventListener('pointercancel', create_proxy(events.append))
 
-    await rpctst_exec_touch_event([
-        {'type': 'touchStart',
-         'touchPoints': [{'x': x, 'y': y, 'radiusX': 2.5, 'radiusY': 2.5, 'rotationAngle': 0, 'force': 1, 'id': 1}],
-         'modifiers': 0},
-        {'type': 'touchMove', 'touchPoints': [
-            {'x': x + 20, 'y': y + 20, 'radiusX': 2.5, 'radiusY': 2.5, 'rotationAngle': 0, 'force': 1, 'id': 1}],
-         'modifiers': 0},
-        {'type': 'touchEnd', 'touchPoints': [], 'modifiers': 0}
-    ])
-    await waitAnimationFrame()
-
-    types = [e.type for e in events]
-    targets = [e.target.id for e in events]
-    assert types == ['pointerdown', 'pointermove', 'pointerup']
-    assert targets == ['div1', 'div1', 'div1']
-    # _verify_xy(events[0], x,     y)
-    # _verify_xy(events[1], x+20, y+20)
-
-
-async def test_touch_on_pointer_api_merl(div1):
-    # GIVEN
-    rect = div1.getBoundingClientRect()
-    x = rect.x + rect.width / 2
-    y = rect.y + rect.height / 2
-    logger.debug(f'GIVEN phase done x={x} y={y}')
-
-    events = []
-    div1.addEventListener('pointerdown', create_proxy(events.append))
-    # div1.addEventListener('pointermove', create_proxy(events.append))
-    div1.addEventListener('pointermove', create_proxy(lambda e: [e.setPointerCapture(e.pointerId), events.append(e)]))
-    div1.addEventListener('pointerup', create_proxy(events.append))
-
     # WHEN
     await rpctst_exec_touch_event([
-        {
-            'type': 'touchStart',
-            'touchPoints': [{
-                'x': x, 'y': y,
-                'radiusX': 2.5, 'radiusY': 2.5,
-                'rotationAngle': 0,
-                'force': 1,
-                'id': 1
-            }],
-            'modifiers': 0
-        },
-        {
-            'type': 'touchMove',
-            'touchPoints': [{
-                'x': x + 20, 'y': y + 20,
-                'radiusX': 2.5, 'radiusY': 2.5,
-                'rotationAngle': 0,
-                'force': 1,
-                'id': 1
-            }],
-            'modifiers': 0
-        },
-        {
-            'type': 'touchEnd',
-            'touchPoints': [{  # Include the last touch point in touchEnd
-                'x': x + 20, 'y': y + 20,
-                'radiusX': 2.5, 'radiusY': 2.5,
-                'rotationAngle': 0,
-                'force': 1,
-                'id': 1
-            }],
-            'modifiers': 0
-        }
+        {'type': 'touchStart', 'touchPoints': [{'x': x, 'y': y}]},
+        {'type': 'touchMove', 'touchPoints': [{'x': x + 5, 'y': y + 5}]},  # smaller movement
+        {'type': 'touchEnd', 'touchPoints': []},
     ])
-
-    # Add a small delay to ensure all events are processed
-    await asyncio.sleep(0.1)
 
     # THEN
     types = list(map(lambda e: e.type, events))
+    logger.debug(f'types={types}')
     assert types == ['pointerdown', 'pointermove', 'pointerup']
     targets = list(map(lambda e: e.target.id, events))
     assert targets == ['div1', 'div1', 'div1']
-    _verify_xy(events[0], x, y)
-    _verify_xy(events[1], x + 20, y + 20)
-    _verify_xy(events[2], x + 20, y + 20)  # Verify position of pointerup event
+
 
 def _verify_xy(event, x, y):
     __tracebackhide__ = True
     item0 = event.touches.item(0)
     assert item0.clientX == x
     assert item0.clientY == y
-
-
-async def test_touch_start(div1):
-    # GIVEN
-    rect = div1.getBoundingClientRect()
-    x = rect.x + rect.width / 2
-    y = rect.y + rect.height / 2
-    logger.debug(f'GIVEN phase done x={x} y={y}')
-
-    events = []
-    proxy = create_proxy(events.append)
-    div1.addEventListener('touchstart', proxy)
-
-    # WHEN
-    await rpctst_exec_touch_event({'type': 'touchStart', 'touchPoints': [{'x': x, 'y': y}]})
-
-    # THEN
-    assert events != []
-    assert len(events) == 1
-    event = events[0]
-    assert event.type == 'touchstart'
-    assert event.target.id == 'div1'
-    item0 = event.touches.item(0)
-    assert item0.clientX == x
-    assert item0.clientY == y
-    div1.removeEventListener('touchstart', proxy)
-
 
 @pytest.fixture()
 def fixture(clean_document): yield Fixture()
