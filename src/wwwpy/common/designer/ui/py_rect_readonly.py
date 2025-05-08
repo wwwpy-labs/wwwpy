7445f8d41f52273bb905dd2f939e023bcbc7e310
simone <simone.giacomelli@gmail.com>
2025-05-08 09:17:41 +0100

    feat(rect): enhance RectReadOnly with property definitions and improve RectReadOnlyDc implementation

--- src/wwwpy/common/designer/ui/py_rect_readonly.py
+++ src/wwwpy/common/designer/ui/py_rect_readonly.py
@@ -1,26 +1,37 @@
-from dataclasses import dataclass
 from typing import Protocol
 
 
 class RectReadOnly(Protocol):
-    x: float
-    y: float
-    width: float
-    height: float
-    top: float
-    right: float
-    bottom: float
-    left: float
+    """This is intended to be used in common to handle js.DOMRectReadOnly"""
+
+    @property
+    def x(self) -> float: ...
+
+    @property
+    def y(self) -> float: ...
+
+    @property
+    def width(self) -> float: ...
+
+    @property
+    def height(self) -> float: ...
+
+    @property
+    def left(self) -> float: ...
+
+    @property
+    def top(self) -> float: ...
+
+    @property
+    def right(self) -> float: ...
+
+    @property
+    def bottom(self) -> float: ...
 
     def toJSON(self) -> object: ...
 
 
-@dataclass(frozen=True)
 class RectReadOnlyDc(RectReadOnly):
-    x: float
-    y: float
-    width: float
-    height: float
 
     @property
     def left(self) -> float:
@@ -45,8 +56,3 @@ class RectReadOnlyDc(RectReadOnly):
             "width": self.width,
             "height": self.height,
         }
-
-
-def main() -> None:
-    x = RectReadOnlyDc(x=0.0, y=0.0, width=1.0, height=1.0)
-    print(x)
