fc42e58a657f35505779537a4cd2f57df76b6177
simone <simone.giacomelli@gmail.com>
2025-05-15 17:33:50 +0100

    refactor(exitlib): add type hint for on_exit callback parameter

--- src/wwwpy/common/exitlib.py
+++ src/wwwpy/common/exitlib.py
@@ -1,12 +1,13 @@
 import sys
 import weakref
+from typing import Callable
 
 
 class _Obj:
     pass
 
 
-def on_exit(callback):
+def on_exit(callback: Callable[[], None]) -> None:
     frame = sys._getframe(1)
     obj = _Obj()
     frame.f_locals[obj] = obj
