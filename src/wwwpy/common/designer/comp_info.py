bab1dc1de5e569a59aa68e2ce8ef8334f41d8c05
simone <simone.giacomelli@gmail.com>
2025-05-18 13:54:36 +0100

    feat(comp_info): add CompInfo class and iter_comp_info function for component information extraction

---
+++ src/wwwpy/common/designer/comp_info.py
@@ -0,0 +1,36 @@
+from __future__ import annotations
+
+import logging
+from dataclasses import dataclass
+from pathlib import Path
+from typing import Iterator
+
+from wwwpy.common.designer import code_info
+from wwwpy.common.designer.code_strings import html_from_source
+from wwwpy.common.designer.html_parser import CstTree, html_to_tree
+
+logger = logging.getLogger(__name__)
+
+
+@dataclass
+class CompInfo:
+    class_name: str
+    path: Path
+    cst_tree: CstTree
+
+
+def iter_comp_info(path: Path) -> Iterator[CompInfo]:
+    source_code = path.read_text()
+    ci = code_info.info(source_code)
+    return (c for c in (_to_comp_info(source_code, path, cl) for cl in ci.classes) if c is not None)
+
+
+def _to_comp_info(source_code: str, path: Path, cl: code_info.ClassInfo) -> CompInfo | None:
+    class_name = cl.name
+    html = html_from_source(source_code, class_name)
+    if html is None:
+        logger.warning(f'Cannot find html for {class_name} in {path}')
+        return None
+
+    cst_tree = html_to_tree(html)
+    return CompInfo(class_name, path, cst_tree)
---
+++ tests/common/designer/comp_info_test.py
@@ -0,0 +1,30 @@
+from __future__ import annotations
+
+from textwrap import dedent
+
+from tests.common import dyn_sys_path
+from wwwpy.common.designer.comp_info import iter_comp_info, CompInfo
+from wwwpy.common.designer.html_parser import CstTree
+
+
+def test_simple_component(dyn_sys_path):
+    comp1_path = dyn_sys_path.write_module2('comp1.py', dedent(
+        """
+        import wwwpy.common.component as wpc
+        class Comp1(wpc.Component):
+            def init_component(self):
+                self.element.innerHTML = '''<div>hello</div>'''
+        """
+    ))
+
+    target = list(iter_comp_info(comp1_path))
+    assert len(target) == 1
+    assert isinstance(target[0], CompInfo)
+    ci0 = target[0]
+    assert ci0.class_name == 'Comp1'
+    assert ci0.path == comp1_path
+    assert ci0.cst_tree is not None
+    assert isinstance(ci0.cst_tree, CstTree)
+    div = ci0.cst_tree[0]
+    assert div.tag_name == 'div'
+    assert div.content == 'hello'
