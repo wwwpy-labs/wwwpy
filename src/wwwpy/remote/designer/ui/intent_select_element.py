3f09eae0df4053fe212b0cc34b3b07f5a20071e6
simone <simone.giacomelli@gmail.com>
2025-05-30 16:47:01 +0100

    feat(intent_select): simplify on_submit method and remove unused code

--- src/wwwpy/remote/designer/ui/intent_select_element.py
+++ src/wwwpy/remote/designer/ui/intent_select_element.py
@@ -6,12 +6,9 @@ import js
 
 from wwwpy.common.designer.canvas_selection import CanvasSelection
 from wwwpy.common.injectorlib import injector
-from wwwpy.remote.designer.helpers import _element_path_lbl
-from wwwpy.remote.designer.locator_js import locator_from
 from wwwpy.remote.designer.ui.element_selector import ElementSelector
 from wwwpy.remote.designer.ui.intent import Intent
 from wwwpy.remote.designer.ui.locator_event import LocatorEvent
-from wwwpy.remote.designer.ui.property_editor import _rebase_element_path_to_origin_source
 
 logger = logging.getLogger(__name__)
 
@@ -28,11 +25,9 @@ class SelectElementIntent(Intent):
 
     def on_submit(self, event: LocatorEvent) -> bool:
         logger.debug(f'on_submit {event}')
-        target = self._set_selection_from_js_event(event)
-        has_target = target is not None
-        if has_target:
-            self._set_toolbox_selection(target)
-        return has_target
+        self._set_selection_from_js_event(event)
+        injector.get(CanvasSelection).current_selection = event.locator
+        return True
 
     def _set_selection_from_js_event(self, le: LocatorEvent):
         element_selector: ElementSelector = self._element_selector
@@ -47,44 +42,6 @@ class SelectElementIntent(Intent):
             element_selector.set_selected_element(target)
 
         return target
-        # old
-        # event = intent_event.js_event
-        # target = intent_event.deep_target
-        # if target is None:
-        #     logger.warning(f'set_selection: target is None {dict_to_py(event)}')
-        #     return
-        #
-        # # element_selector: ElementSelector = injector.get(ElementSelector)
-        #
-        # if not element_selector.is_selectable(target):
-        #     logger.warning(f'set_selection: target is not selectable because o element_selector.is_selectable')
-        #     return
-        #
-        # # unselectable = is_contained(target, self._palette.element)
-        # unselectable = not is_selectable(intent_event)
-        # if unselectable:
-        #     logger.debug(f'set_selection: target is unselectable')
-        #
-        # if unselectable or target == js.document.body or target == js.document.documentElement:
-        #     target = None
-        #
-        # if element_selector.get_selected_element() == target:
-        #     return target
-        # logger.debug(f'set_selection: {_pretty(target)}, unselectable: {unselectable}')
-        # element_selector.set_selected_element(target)
-        # return target
-
-    def _set_toolbox_selection(self, target):
-        ep_live = locator_from(target)
-        logger.debug(f'Element path live: {ep_live}')
-        ep_source = _rebase_element_path_to_origin_source(ep_live)
-        logger.debug(f'Element path source: {ep_source}')
-        message = 'ep_source is none' if ep_source is None else f'ep_source: {_element_path_lbl(ep_source)}'
-        logger.warning(message)
-        if ep_source is not None:
-            injector.get(CanvasSelection).current_selection = ep_source
-        else:
-            logger.warning(message)
 
     @cached_property
     def _element_selector(self) -> ElementSelector:
