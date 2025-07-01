b48306b7f026d1afb1b85dcf4a421e8fb7d1c15a
simone <simone.giacomelli@gmail.com>
2025-07-01 12:12:01 +0100

    Add ObservableList class and integrate its tests
    Introduce `ObservableList` in `collectionlib.py`, an extension of `UserList` with event hooks for item additions and removals. Migrate relevant tests from a separate file to ensure functionality and coverage within the updated structure.

--- src/wwwpy/common/collectionlib.py
+++ src/wwwpy/common/collectionlib.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import Callable, TypeVar, Any, Generic, Collection
+from typing import Callable, TypeVar, Collection
 
 T = TypeVar('T')
 K = TypeVar('K')
@@ -35,3 +35,85 @@ class ListMap(list[T]):
     def get(self, key: K) -> T | None:
         """Return the item with the given key or None if it does not exist."""
         return self._map.get(key)
+
+
+from collections import UserList
+
+
+class ObservableList(UserList):
+    def _item_added(self, item, index):
+        pass
+
+    def _item_removed(self, item, index):
+        pass
+
+    def append(self, item):
+        super().append(item)
+        self._item_added(item, len(self.data) - 1)
+
+    def extend(self, items):
+        start = len(self.data)
+        super().extend(items)
+        for i, item in enumerate(items, start):
+            self._item_added(item, i)
+
+    def insert(self, index, item):
+        super().insert(index, item)
+        self._item_added(item, index)
+
+    def remove(self, item):
+        idx = self.data.index(item)
+        super().remove(item)
+        self._item_removed(item, idx)
+
+    def pop(self, index=-1):
+        idx = index if index >= 0 else len(self.data) + index
+        item = super().pop(index)
+        self._item_removed(item, idx)
+        return item
+
+    def clear(self):
+        for i, item in enumerate(self.data):
+            self._item_removed(item, i)
+        super().clear()
+
+    def __setitem__(self, index, value):
+        if isinstance(index, slice):
+            old = self.data[index]
+            super().__setitem__(index, value)
+            for i, item in enumerate(value, index.start or 0):
+                self._item_added(item, i)
+            for i, item in enumerate(old, index.start or 0):
+                self._item_removed(item, i)
+        else:
+            idx = index if index >= 0 else len(self.data) + index
+            old = self.data[index]
+            super().__setitem__(index, value)
+            self._item_removed(old, idx)
+            self._item_added(value, idx)
+
+    def __delitem__(self, index):
+        if isinstance(index, slice):
+            for i, item in enumerate(self.data[index], index.start or 0):
+                self._item_removed(item, i)
+        else:
+            idx = index if index >= 0 else len(self.data) + index
+            item = self.data[index]
+            self._item_removed(item, idx)
+        super().__delitem__(index)
+
+    def __iadd__(self, other):
+        self.extend(other)
+        return self
+
+    def __imul__(self, n):
+        original = list(self.data)
+        for _ in range(n - 1):
+            self.extend(original)
+        return self
+
+    def __add__(self, other):
+        new = type(self)(self.data + list(other))
+        for i, item in enumerate(other, len(self.data)):
+            new._item_added(item, i)
+        return new
--- tests/common/collectionlib_test.py
+++ tests/common/collectionlib_test.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 
 from wwwpy.common import collectionlib as cl
+from wwwpy.common.collectionlib import ObservableList
 
 
 @dataclass
@@ -25,6 +26,7 @@ def test_add_item():
     assert len(target) == 1
     assert target.get('apple').color == 'red'
 
+
 def test_insert_item():
     # GIVEN
     target = MyListMap()
@@ -35,6 +37,7 @@ def test_insert_item():
     # THEN
     assert len(target) == 1
     assert target.get('apple').color == 'red'
+
 
 def test_extend_items():
     # GIVEN
@@ -47,6 +50,7 @@ def test_extend_items():
     assert len(target) == 2
     assert target.get('apple').color == 'red'
     assert target.get('banana').color == 'yellow'
+
 
 def test_items_in_constructor():
     # GIVEN
@@ -75,3 +79,161 @@ def test_equality_with_list():
 
     # THEN
     assert target == ['a', 'b', 'c']
+
+
+########### ObservableList test
+
+class ObservableListFake(ObservableList):
+    def __init__(self, *args):
+        super().__init__(*args)
+        self.added = []
+        self.removed = []
+
+    def _item_added(self, item, index):
+        self.added.append((item, index))
+
+    def _item_removed(self, item, index):
+        self.removed.append((item, index))
+
+
+class TestObservableList:
+    def test_initialization_empty(self):
+        ol = ObservableListFake()
+        assert ol == []
+        assert ol.added == []
+        assert ol.removed == []
+
+    def test_initialization_with_data(self):
+        ol = ObservableListFake([1, 2, 3])
+        assert ol == [1, 2, 3]
+        assert ol.added == []
+        assert ol.removed == []
+
+    def test_append(self):
+        ol = ObservableListFake()
+        ol.append('a')
+        assert ol == ['a']
+        assert ol.added == [('a', 0)]
+        assert ol.removed == []
+
+    def test_extend(self):
+        ol = ObservableListFake()
+        ol.extend(['a', 'b'])
+        assert ol == ['a', 'b']
+        assert ol.added == [('a', 0), ('b', 1)]
+
+    def test_insert(self):
+        ol = ObservableListFake([1, 3])
+        ol.insert(1, 2)
+        assert ol == [1, 2, 3]
+        assert ol.added == [(2, 1)]
+
+    def test_insert_at_beginning(self):
+        ol = ObservableListFake([2, 3])
+        ol.insert(0, 1)
+        assert ol == [1, 2, 3]
+        assert ol.added == [(1, 0)]
+
+    def test_remove(self):
+        ol = ObservableListFake([1, 2, 3])
+        ol.remove(2)
+        assert ol == [1, 3]
+        assert ol.removed == [(2, 1)]
+
+    def test_pop(self):
+        ol = ObservableListFake([1, 2, 3])
+        item = ol.pop()
+        assert item == 3
+        assert ol == [1, 2]
+        assert ol.removed == [(3, 2)]
+
+    def test_pop_with_index(self):
+        ol = ObservableListFake([1, 2, 3])
+        result = ol.pop(0)
+        assert result == 1
+        assert ol == [2, 3]
+        assert ol.removed == [(1, 0)]
+
+    def test_clear(self):
+        ol = ObservableListFake([1, 2])
+        ol.clear()
+        assert ol == []
+        assert ol.removed == [(1, 0), (2, 1)]
+
+    def test_setitem_single(self):
+        ol = ObservableListFake([1, 2, 3])
+        ol[1] = 'x'
+        assert ol == [1, 'x', 3]
+        assert ol.removed == [(2, 1)]
+        assert ol.added == [('x', 1)]
+
+    def test_setitem_slice(self):
+        ol = ObservableListFake([1, 2, 3, 4])
+        ol[1:3] = ['a', 'b']
+        assert ol == [1, 'a', 'b', 4]
+        assert ('a', 1) in ol.added and ('b', 2) in ol.added
+        assert (2, 1) in ol.removed and (3, 2) in ol.removed
+
+    def test_delitem_single(self):
+        ol = ObservableListFake([1, 2, 3])
+        del ol[1]
+        assert ol == [1, 3]
+        assert ol.removed == [(2, 1)]
+
+    def test_delitem_slice(self):
+        ol = ObservableListFake([1, 2, 3, 4])
+        del ol[1:3]
+        assert ol == [1, 4]
+        assert (2, 1) in ol.removed and (3, 2) in ol.removed
+
+    def test_iadd(self):
+        ol = ObservableListFake([1])
+        ol += [2, 3]
+        assert ol == [1, 2, 3]
+        assert ol.added[-2:] == [(2, 1), (3, 2)]
+
+    def test_imul(self):
+        ol = ObservableListFake([1, 2])
+        ol *= 3
+        assert ol == [1, 2, 1, 2, 1, 2]
+        assert ol.added == [(1, 2), (2, 3), (1, 4), (2, 5)]
+
+    def test_add(self):
+        ol = ObservableListFake([1])
+        new = ol + [2]
+        assert isinstance(new, ObservableListFake)
+        assert new == [1, 2]
+        assert new.added == [(2, 1)]
+
+    def test_complex_operations_sequence(self):
+        ol = ObservableListFake()
+        ol.append(1)
+        ol.extend([2, 3])
+        ol.insert(1, 99)
+        ol[0] = 100
+        assert ol == [100, 99, 2, 3]
+        expected_added = [(1, 0), (2, 1), (3, 2), (99, 1), (100, 0)]
+        expected_removed = [(1, 0)]
+        assert ol.added == expected_added
+        assert ol.removed == expected_removed
+
+    def test_edge_case_empty_extend(self):
+        ol = ObservableListFake([1, 2])
+        ol.extend([])
+        assert ol == [1, 2]
+        assert ol.added == []
+        assert ol.removed == []
+
+    def test_edge_case_empty_slice_assignment(self):
+        ol = ObservableListFake([1, 2, 3])
+        ol[1:1] = [99]
+        assert ol == [1, 99, 2, 3]
+        assert ol.added == [(99, 1)]
+        assert ol.removed == []
+
+    def test_negative_indexing(self):
+        ol = ObservableListFake([1, 2, 3])
+        ol[-1] = 99
+        assert ol == [1, 2, 99]
+        assert ol.added == [(99, 2)]
+        assert ol.removed == [(3, 2)]
