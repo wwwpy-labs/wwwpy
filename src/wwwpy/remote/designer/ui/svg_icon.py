a54e18f53dfeba0b134f791f18a53855bfe62c21
simone <simone.giacomelli@gmail.com>
2025-07-11 11:13:15 +0100

    Refactor SVG icon styling and remove border attribute.
    Replaced the `border` attribute with a more flexible `host_style` dictionary to manage styles dynamically. Refactored the `_set_style` method for improved clarity and maintainability, added logging, and introduced a `connectedCallback` method to set styles when the element is attached. Simplified the `from_file` method by removing the optional border parameter.

--- src/wwwpy/remote/designer/ui/svg_icon.py
+++ src/wwwpy/remote/designer/ui/svg_icon.py
@@ -21,13 +21,11 @@ class SvgIcon(wpc.Component, tag_name='wwwpy-svg-icon'):
     _style: js.HTMLStyleElement = wpc.element()
     _div: js.HTMLDivElement = wpc.element()
     _active: bool
-    border: int = 5
+    host_style: dict[str, str]
 
     @classmethod
-    def from_file(cls, file: Path, border: int | None = None) -> SvgIcon:
+    def from_file(cls, file: Path) -> SvgIcon:
         r = cls()
-        if border is not None:
-            r.border = border
         r.load_svg_str(file.read_text())
         r.element.setAttribute('title', file.name)
         return r
@@ -36,11 +34,44 @@ class SvgIcon(wpc.Component, tag_name='wwwpy-svg-icon'):
         self.element.attachShadow(dict_to_js({'mode': 'open'}))
         # language=html
         self.element.shadowRoot.innerHTML = """
-<style data-name="_style"></style>
+<style data-name="_style">
+:host { display: flex; border: 0px solid transparent ; --svg-primary-color: #2B2D30;  }
+:host(:hover) { --svg-primary-color: #3C3E41; }
+svg { display: block }
+</style>
 <div data-name="_div"></div>
         """
-
         self.active = False
+        self.host_style: dict[str, str] = dict()
+        self.host_style['border'] = '5px solid transparent'
+
+    def _set_style(self, source='na'):
+        color, hover_color = (_BLUE, '') if self.active else (_BGRD, _GRAY)
+
+        logger.warning(f'_set_style: {source}')
+        sheet = self._style.sheet
+
+        js.console.log('=' * 10, self._style, sheet)
+        if not sheet or not sheet.cssRules:
+            logger.warning('No CSS rules found in the style element.')
+            return
+
+        cssRules = list(self._style.sheet.cssRules)
+        host_normal = cssRules[0]
+        host_normal.style.setProperty('--svg-primary-color', color)
+        for key, value in self.host_style.items():
+            host_normal.style.setProperty(key, value)
+
+        host_hover = cssRules[1]
+        host_hover.style.setProperty('--svg-primary-color', hover_color)
+        return
+        # language=html
+        hover_style = ":host(:hover) { --svg-primary-color: %s; }" % (hover_color,) if hover_color else ''
+        s = ('svg { display: block }\n' +
+             ':host { display: flex; border: %spx solid transparent }\n' % self.border +
+             ':host { --svg-primary-color: %s; }\n' % color + hover_style)
+        logger.debug(f'set_style: `{s}`')
+        self._style.innerHTML = s
 
     def load_svg_str(self, svg: str):
         self._div.innerHTML = add_rounded_background2(svg, 'var(--svg-primary-color)')
@@ -55,17 +86,10 @@ class SvgIcon(wpc.Component, tag_name='wwwpy-svg-icon'):
     @active.setter
     def active(self, value: bool):
         self._active = value
-        self._set_style()
+        self._set_style('active')
 
-    def _set_style(self):
-        color, hover_color = (_BLUE, '') if self.active else (_BGRD, _GRAY)
-        # language=html
-        hover_style = ":host(:hover) { --svg-primary-color: %s; }" % (hover_color,) if hover_color else ''
-        s = ('svg { display: block }\n' +
-             ':host { display: flex; border: %spx solid transparent }\n' % self.border +
-             ':host { --svg-primary-color: %s; }\n' % color + hover_style)
-        logger.debug(f'set_style: `{s}`')
-        self._style.innerHTML = s
+    def connectedCallback(self):
+        self._set_style('connectedCallback')
 
     def _div__click(self, event):
         self.active = not self.active
