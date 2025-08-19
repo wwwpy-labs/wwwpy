4aa37cbfd8976b9bc42c4bef545cce9fb0de6d0c
simone <simone.giacomelli@gmail.com>
2025-08-19 12:56:53 +0100

    Update chat setup to use simple_dark_theme
    Replaced shoelace.setup_shoelace() with simple_dark_theme.setup() in the chat's remote initialization. This ensures compatibility with the updated UI theming approach. Additional components remain unaffected.

--- src/wwwpy/common/quickstart/chat/remote/__init__.py
+++ src/wwwpy/common/quickstart/chat/remote/__init__.py
@@ -2,8 +2,8 @@ from js import document
 
 
 async def main():
-    from wwwpy.remote import shoelace
-    shoelace.setup_shoelace()
+    from wwwpy.remote import simple_dark_theme
+    simple_dark_theme.setup()
 
     from . import component1  # for component registration
     document.body.innerHTML = '<component-1></component-1>'
